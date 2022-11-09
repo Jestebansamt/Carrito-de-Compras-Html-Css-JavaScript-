d76a8e3884bbfa5db48d995dbcb4a68c89f253cd
Jestebansamt <64326488+Jestebansamt@users.noreply.github.com>
2022-11-08 21:03:11 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("#header");
+const contenedor = document.querySelector("#contenedor");
+const body = document.querySelector("body");
+window.addEventListener("scroll", function(){
+    if(contenedor.getBoundingClientRect().top<10){
+        header.classList.add("scroll")
+    }
+    else{
+        header.classList.remove("scroll")
+    }
+})
